dec1ba1935204299a352ec0da1b5b76a1874dacf
Henrique Bandeira <henriquebandeira307@gmail.com>
2022-10-04 12:05:31 -0300

    Prova Finalizada

--- Prova1/ParImpar/ParImpar.java
+++ Prova1/ParImpar/ParImpar.java
@@ -1,5 +1,46 @@
 package Prova1.ParImpar;
 
+/*
+2) (3,0). Classe ParImpar. 
+Faça uma classe contendo dois arrays do tipo inteiro, 
+um chamado par e outro chamado ímpar. 
+Cada array deve conter 10 elementos.
+
+Popule esses arrays através de uma geração randômica de números entre 1 e 30,
+categorizando os valores pares e ímpares. 
+
+Ao final imprima na tela a lista de número pares e a lista de números ímpares.
+*/
+
+import java.util.Random;
+
 public class ParImpar {
-    
+    public static void main(String[] args) {
+
+        int tamanho = 10;
+        int Impar[] = new int[tamanho];
+        int Par[] = new int[tamanho];
+        int aux=0;
+        Random gerador = new Random();
+
+        int par=0,impar=0;
+
+        do{
+            aux = gerador.nextInt(30);
+
+            if (aux%2 ==0  && par<10){
+                Par[par] = aux;
+                par++;
+            }else if( aux%2 !=0  && impar<10){
+                Impar[impar] = aux;
+                impar++;
+            }
+        }while(impar<10  && par<10);
+
+        for (int i=0; i<tamanho;i++) {
+            System.out.printf("\nO valor(%s) impar: %s \n",i+1, Impar[i]);
+
+            System.out.printf("\nO valor(%s) par: %s \n",i+1, Par[i]);
+        }
+    }
 }
--- Prova1/Questão3/UrnaDeVotacao.java
+++ Prova1/Questão3/UrnaDeVotacao.java
@@ -4,45 +4,63 @@ public class UrnaDeVotacao {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String s, ganhador;
-		String entrada;
-		int count1=0,count2=0,count3=0, totalVotos=0, votoInvalido=0; 
+		Scanner sc2 = new Scanner(System.in);
+		String entrada, ganhador;
+		int votos[] = new int[3],totalVotos = 0, votoInvalido=0; 
+		float qtdVotos = 0;
 		
 		System.out.println("Diga quantas pessoas vão votar: ");
 		int nmrPessoas = sc.nextInt();
 
-		
+		// for(int i = 0;i < votos.length;i++){
+		// 	votos[i] = 0;
+		// }
+
 		do{	
 			System.out.println("Digite o código de seu candidato \n"+
 				"    111 - Maria da Silva \n" +
 				"    222 - Pedro de Araújo \n" +
 				"    333 - José de Souza \n");
+			entrada = sc2.nextLine();
 		
-			if (entrada.equals("111")) {
-				count1++;
-			}else if (entrada.equals("222")){
-				count2++;
+			switch (entrada) {
+				case "111":
+					votos[0]++;
+				break;
+				case "222":
+					votos[1]++;
+				break;
+				case "333":
+					votos[2]++;
+				break;
+				default:
+					votoInvalido++;
+					break;
 			}
-			else if(entrada.equals("333")){
-				count3++;
-			}else{
-				votoInvalido++;
-			}
-		}while(contador != nmrPessoas);
+			qtdVotos++;
+			nmrPessoas--;
+		}while(nmrPessoas != 0);
 			
-		if (count1>count2 && count1>count3) {
+		if (votos[0]>votos[1] && votos[0]>votos[2]) {
 			ganhador = "Maria Silva";
-			totalVotos = count1;
-		}else if (count2>count3) {
+			totalVotos = votos[0];
+		}else if (votos[1]>votos[2]) {
 				ganhador = "Pedro Araújo";
-				totalVotos=count2;
+				totalVotos=votos[1];
 		}else 
 		{	ganhador = "José de Souza";
-			totalVotos=count3;
+			totalVotos=votos[2];
 		}
 		
-		JOptionPane.showMessageDialog(null, "O ganhador foi "+ganhador+" com "
+		System.out.println("\nTotal de votos: "+qtdVotos);
+		System.out.println("\nO ganhador foi "+ganhador+" com "
 				+totalVotos + " votos");
+		System.out.println("\nVotos inválidos foram: "+(votoInvalido/qtdVotos)*100+"%"+
+		"\nVotos válidos foram: "+((qtdVotos-votoInvalido)/qtdVotos)*100+"%");
+		
+		for(int i = 0;i < 3;i++){
+			System.out.println("\nO percentual do canditado "+(i+1)+" foi de: "+(votos[i]/qtdVotos)*100+"%");
+		}
 		
 		System.exit(0);		
 	}
